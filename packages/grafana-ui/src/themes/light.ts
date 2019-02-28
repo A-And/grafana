9ec182b0800fd07ff5c0399a9b69eb49fe294511
Andon Andonov <andon.andonov@microsoft.com>
2019-02-27 19:51:00 -0800

    Preliminary changes

--- packages/grafana-ui/src/themes/light.ts
+++ packages/grafana-ui/src/themes/light.ts
@@ -5,7 +5,7 @@ import { GrafanaTheme, GrafanaThemeType } from '../types/theme';
 const basicColors = {
   black: '#000000',
   white: '#ffffff',
-  dark1: '#1e2028',
+  //dark1: '#1e2028',
   dark2: '#41444b',
   dark3: '#303133', // not used in light theme
   dark4: '#35373f', // not used in light theme
@@ -19,8 +19,8 @@ const basicColors = {
   gray2: '#767980',
   gray3: '#acb6bf',
   gray4: '#c7d0d9',
-  gray5: '#dde4ed',
-  gray6: '#e9edf2',
+  gray5Default: '#dde4ed',
+  //gray6: '#e9edf2',
   gray7: '#f7f8fa',
   grayBlue: '#212327', // not used in light theme
   blueBase: '#3274d9',
@@ -29,7 +29,8 @@ const basicColors = {
   blueFaint: '#f5f9ff',
   redBase: '#e02f44',
   redShade: '#c4162a',
-  greenBase: '#3eb15b',
+  //greenBase: '#3eb15b',
+  greenDefault: '#3eb15b',
   greenShade: '#369b4f',
   blue: '#0083b3',
   red: '#d44939',
@@ -39,40 +40,57 @@ const basicColors = {
   orange: '#ff7941',
 };
 
+const azureColors = {
+  blue: '#0078d4',
+  black: '#000000',
+  blueLight: '#50e6ff',
+  white: '#ffffff',
+  grayDark: '#3c3c41',
+  grayMedium: '#75757a',
+  grayLight: '#ebebeb;',
+};
+
 const lightTheme: GrafanaTheme = {
   ...defaultTheme,
   type: GrafanaThemeType.Light,
   name: 'Grafana Light',
   colors: {
     ...basicColors,
-    variable: basicColors.blue,
+    // Azure Theme Overrides
+    dark1: azureColors.black,
+    greenBase: azureColors.blue,
+    greenShade: azureColors.blueLight,
+    gray5: azureColors.grayLight,
+    gray6: new tinycolor(azureColors.grayLight).lighten(80).toString(),
+    //
+    variable: azureColors.blue,
     inputBlack: '#09090b',
     queryRed: basicColors.red,
-    queryGreen: basicColors.greenBase,
+    queryGreen: basicColors.greenDefault,
     queryPurple: basicColors.purple,
     queryKeyword: basicColors.blue,
-    queryOrange: basicColors.orange,
+    queryOrange: basicColors.blue,
     online: basicColors.greenShade,
     warn: '#f79520',
     critical: basicColors.redShade,
     bodyBg: basicColors.gray7,
     pageBg: basicColors.gray7,
-    bodyColor: basicColors.gray1,
-    textColor: basicColors.gray1,
+    bodyColor: azureColors.blueLight,
+    textColor: azureColors.blueLight,
     textColorStrong: basicColors.dark2,
     textColorWeak: basicColors.gray2,
-    textColorEmphasis: basicColors.gray5,
+    textColorEmphasis: basicColors.gray5Default,
     textColorFaint: basicColors.dark4,
-    linkColor: basicColors.gray1,
-    linkColorDisabled: new tinycolor(basicColors.gray1).lighten(30).toString(),
-    linkColorHover: new tinycolor(basicColors.gray1).darken(20).toString(),
+    linkColor: azureColors.blueLight,
+    linkColorDisabled: new tinycolor(azureColors.blueLight).lighten(30).toString(),
+    linkColorHover: new tinycolor(azureColors.blueLight).darken(20).toString(),
     linkColorExternal: basicColors.blueLight,
-    headingColor: basicColors.gray1,
+    headingColor: azureColors.blueLight,
   },
   background: {
     dropdown: basicColors.white,
-    scrollbar: basicColors.gray5,
-    scrollbar2: basicColors.gray5,
+    scrollbar: basicColors.gray5Default,
+    scrollbar2: basicColors.gray5Default,
   },
 };
 
